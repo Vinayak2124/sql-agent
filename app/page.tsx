3e1acdcd917498a841925b579dbf6b0287be6e11
Vinayak Chaube <vinayakchaube24@gmail.com>
2025-10-22 23:54:02 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <H2>Welcome to SQL-Agent</H2>
+      <h2>Welcome to SQL-Agent</h2>
     </div>
       );
 }
